afa56e342434288d5205af72657467d5f7b15f3e
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-08-15 14:01:55 -0300

    começado a fazer as routas de categoria

---
+++ src/http/routes.js
@@ -0,0 +1,32 @@
+
+const routes = (server) => {
+  server.get('/', (req, res, next) => {
+    res.send('Enjoy the silence...')
+    next()
+  })
+
+  server.get('/categoria', (req, res, next) => {
+    res.send(['1', 'lalala'])
+
+    next()
+  })
+  server.post('/categoria', (req, res, next) => {
+    const { name } = req.params
+
+    res.send(name)
+
+    next()
+  })
+  server.put('/categoria', (req, res, next) => {
+    res.send()
+
+    next()
+  })
+  server.del('/categoria', (req, res, next) => {
+    res.send()
+
+    next()
+  })
+}
+
+module.exports = routes
