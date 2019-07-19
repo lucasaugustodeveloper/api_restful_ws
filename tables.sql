6426b0184178d63078e5099b2e54beef583d0e7c
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-19 12:51:31 -0300

    updated command created database and tables

--- tables.sql
+++ tables.sql
@@ -1,8 +1,26 @@
 -- Create database
 create database api_restful_ws default CHARACTER set utf8 DEFAULT COLLATE utf8_general_ci;
-create user 'api_resftul_ws'@'localhost' identified by 'api_resftul_ws';
-grant all on api_resftul_ws.* to 'api_resftul_ws'@'localhost';
+create database api_restful_ws_test default CHARACTER set utf8 DEFAULT COLLATE utf8_general_ci;
+-- create user 'api_resftul_ws'@'localhost' identified by 'api_resftul_ws';
+-- grant all on api_resftul_ws.* to 'api_resftul_ws'@'localhost';
 
+use api_restful_ws;
+-- Create table syntax for TABLE 'categories'
+create table categories (
+    id int(11) unsigned not null auto_increment,
+    name varchar(255) not null,
+    primary key(id)
+);
+
+-- Create table syntax for TABLE 'users'
+create table users (
+    id int(11) unsigned not null auto_increment,
+    email varchar(70) not null,
+    pass varchar(40) not null,
+    primary key(id)
+);
+
+use api_restful_ws_test;
 -- Create table syntax for TABLE 'categories'
 create table categories (
     id int(11) unsigned not null auto_increment,
