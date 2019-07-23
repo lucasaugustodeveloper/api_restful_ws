8208b2ea86d0e52eb6de87302c127c2d695bf5bc
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-22 21:25:27 -0300

    Add function for register user

---
+++ src/http/routes/users/index.route.js
@@ -0,0 +1,24 @@
+const db = require('../../../server/services/mysql')
+const users = (server) => {
+  server.get('/users', async (req, res, next) => {
+    try {
+      res.send(await db.users().all())
+    } catch (error) {
+      res.send(error)
+    }
+
+    next()
+  })
+  server.post('/register', async (req, res, next) => {
+    try {
+      const { email, password } = req.params
+      res.send(await db.users().save(email, password))
+    } catch (error) {
+      res.send(error)
+    }
+
+    next()
+  })
+}
+
+module.exports = users
